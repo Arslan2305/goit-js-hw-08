46c20f9485f2c0fb87871279612cd773a0a6f646
Arslan Ziiatdinov <airtraffic2013@gmail.com>
2023-04-01 16:46:50 +0300

    new

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,29 +6,30 @@ import { galleryItems } from './gallery-items';
 
 console.log(galleryItems);
 
-const galleryRef = document.querySelector('.gallery');
-const itemsMarkap = makeGalleryItems(galleryItems);
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItems(galleryItems);
 
-galleryRef.insertAdjacentHTML('beforeend', itemsMarkap);
+ gallery.insertAdjacentHTML('beforeend', galleryMarkup);
 
-function makeGalleryItems(galleryItems) {
-  return galleryItems
-    .map(({ preview, original, description }) => {
-      return `
-      <a class="gallery__item" href="${original}">
-        <img
-          class="gallery__image"
-          src="${preview}"
-          alt="${description}"
-        />
-      </a>
-    `;
-    })
-    .join('');
-}
-const lightbox = new SimpleLightbox('.gallery a', {
+function createGalleryItems(galleryItems) {
+  return galleryItems.map(({original, preview, description}) => {
+    return `<li class="gallery__item">
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+}).join(''); 
+};
+
+  const lightbox = new SimpleLightbox(".gallery a", {
   captions: true,
-  captionsData: 'alt',
-  captionPosition: 'bottom',
+  captionsData: "alt",
+  captionPosition: "bottom",
   captionDelay: 250,
-});
+  });
+
+  
